b9960e34384a3f43bdaafa920790857996ed383b
DC <dc5stars@gmail.com>
2023-01-31 17:30:57 -0500

    Old server uri parameter deleted

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import Pages from "./pages";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  //uri: "https://space-courses-api.herokuapp.com/", // change to YOUR own production server
   uri: "https://odyssey-lift-off-part5-server-production-75c1.up.railway.app/",
   cache: new InMemoryCache(),
   name: "web",
